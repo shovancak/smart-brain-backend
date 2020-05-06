a01be00aa47c3c52792388b365ab49ace2908182
shovancak <s.hovancak@gmail.com>
2020-05-06 13:06:06 +0200

    repair PORT

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`app is running on port ${process.eventNames.PORT}`);
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
